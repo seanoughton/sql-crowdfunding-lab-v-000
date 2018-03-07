28a36aa6a038be7fbd38035350639809b4a452d3
Sean Oughton <seanoughton@gmail.com>
2018-03-07 22:56:45 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,7 +2,7 @@ CREATE TABLE projects (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   title TEXT,
   category TEXT,
-  funding_goal TEXT,
+  funding_goal INTEGER,
   start_date TEXT,
   end_date TEXT
 );

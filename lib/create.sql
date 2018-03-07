541319a3b80f20ca78a600f8cc2c2ba4bd3ddeff
Sean Oughton <seanoughton@gmail.com>
2018-03-07 22:00:42 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,19 +1,1 @@
-CREATE TABLE projects (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	title TEXT,
-  category TEXT,
-	funding_goal INTEGER
-);
 
-CREATE TABLE users (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	name TEXT,
-	age INTEGER
-);
-
-CREATE TABLE pledges (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	amount INTEGER,
-  user_id = INTEGER,
-  project_id = INTEGER
-);
